92ee81cbe921c4fe1ca67bfc64dd2d2fa4238562
22-YanKaiky <yankaikys@gmail.com>
2021-09-29 00:49:52 -0300

    variaves para arrays

--- filter/index.js
+++ filter/index.js
@@ -1,4 +1,4 @@
-valorTotal - [1, 4, 6, 6, 3, 434, 65, 56, 4, 3, 32, 65, 4]
+let valorTotal = [1, 4, 6, 6, 3, 434, 65, 56, 4, 3, 32, 65, 4]
 
 var semRepetidos = valorTotal.filter(function(el, i) {
    return valorTotal.indexOf(el) === i;
